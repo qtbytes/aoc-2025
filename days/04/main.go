58e6b4b187d7d3d7dfdee1fe26439c41ade02a1e
qt <qtbytes@outlook.com>
2025-12-07 21:14:24 +0800

    Part 2: BFS

--- days/04/main.go
+++ days/04/main.go
@@ -6,19 +6,27 @@ import (
 	"os"
 )
 
+type Pair struct {
+	i, j int
+}
+
+type Item struct {
+	i, j, cnt int
+}
+
 func main() {
 	// f, _ := os.Open("days/04/sample.txt")
 	f, _ := os.Open("days/04/input.txt")
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	var board []string
+	var board [][]rune
 	for scanner.Scan() {
-		board = append(board, scanner.Text())
+		board = append(board, []rune(scanner.Text()))
 	}
-	fmt.Println(board)
-	ans := 0
 	m, n := len(board), len(board[0])
+	var q []Item
+	adj := make(map[Pair]int) // count
 	for i, s := range board {
 		for j, ch := range s {
 			if ch != '@' {
@@ -33,9 +41,58 @@ func main() {
 				}
 			}
 			if cnt < 4 {
-				ans++
+				q = append(q, Item{i, j, cnt})
+			}
+			adj[Pair{i, j}] = cnt
+		}
+	}
+	ans := 0
+	for len(q) > 0 {
+		item := q[0]
+		q = q[1:]
+		i, j := item.i, item.j
+		if board[i][j] != '@' {
+			continue
+		}
+		ans++
+		board[i][j] = 'x'
+		for x := max(0, i-1); x < min(m, i+2); x++ {
+			for y := max(0, j-1); y < min(n, j+2); y++ {
+				if x == i && y == j {
+					continue
+				}
+				if board[x][y] == '@' {
+					adj[Pair{x, y}]--
+					if adj[Pair{x, y}] < 4 {
+						q = append(q, Item{x, y, adj[Pair{x, y}]})
+					}
+				}
 			}
 		}
 	}
 	fmt.Println(ans)
+
+	// Part 1
+	//
+	// ans := 0
+	// m, n := len(board), len(board[0])
+	// for i, s := range board {
+	// 	for j, ch := range s {
+	// 		if ch != '@' {
+	// 			continue
+	// 		}
+	// 		cnt := -1 // will count self again
+	// 		for x := max(0, i-1); x < min(m, i+2); x++ {
+	// 			for y := max(0, j-1); y < min(n, j+2); y++ {
+	// 				if board[x][y] == '@' {
+	// 					cnt++
+	// 				}
+	// 			}
+	// 		}
+	// 		if cnt < 4 {
+	// 			ans++
+	// 		}
+	// 	}
+	// }
+	// fmt.Println(ans)
 }
