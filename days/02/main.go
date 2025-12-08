fc19887521f7e8432a46687949bae634004ead87
qt <qtbytes@outlook.com>
2025-12-08 17:09:31 +0800

    Part 2: Math

--- days/02/main.go
+++ days/02/main.go
@@ -3,33 +3,59 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+// Part 1
+//
+//	func handle(x, y int) (ans int) {
+//		s := strconv.Itoa(x)
+//		var start int
+//		if len(s)&1 == 1 {
+//			start = int(math.Pow10(len(s) / 2))
+//		} else {
+//			start, _ = strconv.Atoi(s[:len(s)/2])
+//		}
+//		for {
+//			z, _ := strconv.Atoi(strings.Repeat(strconv.Itoa(start), 2))
+//			if z > y {
+//				break
+//			}
+//			if z >= x {
+//				ans += z
+//			}
+//			start++
+//		}
+//		return ans
+//	}
+
 func handle(x, y int) (ans int) {
 	s := strconv.Itoa(x)
-	var start int
-	if len(s)&1 == 1 {
-		start = int(math.Pow10(len(s) / 2))
-	} else {
-		start, _ = strconv.Atoi(s[:len(s)/2])
-	}
-	for {
-		z, _ := strconv.Atoi(strings.Repeat(strconv.Itoa(start), 2))
-		if z > y {
-			break
+	seen := make(map[int]bool)
+	// repeat times
+	for _, t := range []int{2, 3, 5, 7} {
+		start, _ := strconv.Atoi(s[:len(s)/t])
+		for {
+			z, _ := strconv.Atoi(strings.Repeat(strconv.Itoa(start), t))
+			if z > y {
+				break
+			}
+			if z >= x {
+				// fmt.Println(x, y, z)
+				seen[z] = true
+			}
+			start++
 		}
-		if z >= x {
-			ans += z
-		}
-		start++
 	}
-	return ans
-
+	for x := range seen {
+		fmt.Println(x)
+		ans += x
+	}
+	return
 }
+
 func main() {
 	// f, _ := os.Open("days/02/sample.txt")
 	f, _ := os.Open("days/02/input.txt")
