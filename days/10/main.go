f2879cfacaec539eff3a0c09f17afc44731e419c
qt <qtbytes@outlook.com>
2025-12-10 22:19:03 +0800

    Part 2: Unsolve

--- days/10/main.go
+++ days/10/main.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"math/bits"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-func handle(lights string, buttons []string) int {
+func handle1(lights string, buttons []string) int {
 	// we know each button most be pressed once
 	target := 0
 	lights = lights[1 : len(lights)-1]
@@ -53,14 +54,78 @@ func part1(scanner *bufio.Scanner) {
 		n := len(line)
 		lights := line[0]
 		buttons := line[1 : n-1]
-		ans += handle(lights, buttons)
+		ans += handle1(lights, buttons)
 	}
 	fmt.Println(ans)
 }
 
+func handle2(buttons []string, joltage string) int {
+	joltage = joltage[1 : len(joltage)-1]
+	var target []int
+	for num := range strings.SplitSeq(joltage, ",") {
+		num, _ := strconv.Atoi(num)
+		target = append(target, num)
+	}
+
+	var pairs [][]int
+	for _, button := range buttons {
+		var pair []int
+		for num := range strings.SplitSeq(button[1:len(button)-1], ",") {
+			num, _ := strconv.Atoi(num)
+			pair = append(pair, num)
+		}
+		pairs = append(pairs, pair)
+	}
+
+	var dfs func(i int, cnt []int) int
+	dfs = func(i int, cnt []int) int {
+		if i == len(pairs) {
+			if slices.Equal(cnt, target) {
+				return 0
+			} else {
+				return 100
+			}
+		}
+		ans := dfs(i+1, cnt)
+		for j := 1; ; j++ {
+			ok := true
+			newCnt := slices.Clone(cnt)
+			for _, x := range pairs[i] {
+				newCnt[x] += j
+				if newCnt[x] > target[x] {
+					ok = false
+					break
+				}
+			}
+			if !ok {
+				break
+			}
+			ans = min(ans, j+dfs(i+1, newCnt))
+		}
+		return ans
+	}
+
+	cnt := make([]int, len(target))
+	ans := dfs(0, cnt)
+	fmt.Println(target, pairs, ans)
+	return ans
+}
+
+func part2(scanner *bufio.Scanner) {
+	ans := 0
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		n := len(line)
+		buttons := line[1 : n-1]
+		cnt := line[n-1]
+		ans += handle2(buttons, cnt)
+	}
+	fmt.Println(ans)
+}
 func main() {
 	// f, _ := os.Open("days/10/sample.txt")
 	f, _ := os.Open("days/10/input.txt")
 	scanner := bufio.NewScanner(f)
-	part1(scanner)
+	// part1(scanner)
+	part2(scanner)
 }
